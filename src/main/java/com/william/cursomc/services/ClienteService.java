b63813b9fb96d2c1488760808295cced32809748
WilliamWJD <william.jose.wjd@gmail.com>
2023-06-05 09:36:14 -0300

    feat: criando um validador personalizado para cpf e cnpj

--- src/main/java/com/william/cursomc/services/ClienteService.java
+++ src/main/java/com/william/cursomc/services/ClienteService.java
@@ -55,7 +55,7 @@ public class ClienteService {
 		try {
 			clienteRepository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir por que há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir por que há pedidos relacionados");
 		}
 	}
 
