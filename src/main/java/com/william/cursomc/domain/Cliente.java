f728a7bc5e3130ca1991406d3b6445151ddb2501
WilliamWJD <william.jose.wjd@gmail.com>
2023-06-05 09:46:00 -0300

    feat: validação customizada para duplicidade de e-mail cliente

--- src/main/java/com/william/cursomc/domain/Cliente.java
+++ src/main/java/com/william/cursomc/domain/Cliente.java
@@ -12,6 +12,7 @@ import com.william.cursomc.domain.enums.TipoCliente;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -27,7 +28,10 @@ public class Cliente implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@Column(unique = true)
 	private String email;
+	
 	private String cpfOuCnpj;
 	private Integer tipo;
 
--- src/main/java/com/william/cursomc/services/validations/ClienteInsertValidator.java
+++ src/main/java/com/william/cursomc/services/validations/ClienteInsertValidator.java
@@ -3,8 +3,12 @@ package com.william.cursomc.services.validations;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.william.cursomc.domain.Cliente;
 import com.william.cursomc.domain.enums.TipoCliente;
 import com.william.cursomc.dto.ClienteNewDTO;
+import com.william.cursomc.repositories.ClienteRepository;
 import com.william.cursomc.resources.exceptions.FieldMessage;
 import com.william.cursomc.services.validations.utils.BR;
 
@@ -12,6 +16,9 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -27,6 +34,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(cliente != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
