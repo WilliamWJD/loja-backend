e1501e3bd501349efba9084f95aa5d3d9b089abc
WilliamWJD <william.jose.wjd@gmail.com>
2023-06-14 23:20:39 -0300

    feat: implementando autorização de requisição nas config do security

--- src/main/java/com/william/cursomc/config/SecurityConfig.java
+++ src/main/java/com/william/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.william.cursomc.security.JWTAuthenticationFilter;
+import com.william.cursomc.security.JWTAuthorizationFilter;
 import com.william.cursomc.security.JWTUtil;
 
 @Configuration
@@ -50,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
