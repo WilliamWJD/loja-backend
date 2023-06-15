a7b973074171bb08305f5e346b5553e707474172
WilliamWJD <william.jose.wjd@gmail.com>
2023-06-15 09:37:35 -0300

    feat: cliente só recupera seus pedidos

--- src/main/java/com/william/cursomc/services/PedidoService.java
+++ src/main/java/com/william/cursomc/services/PedidoService.java
@@ -3,16 +3,24 @@ package com.william.cursomc.services;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.william.cursomc.domain.Categoria;
+import com.william.cursomc.domain.Cliente;
 import com.william.cursomc.domain.ItemPedido;
 import com.william.cursomc.domain.PagamentoComBoleto;
 import com.william.cursomc.domain.Pedido;
 import com.william.cursomc.domain.enums.EstadoPagamento;
+import com.william.cursomc.domain.enums.Perfil;
 import com.william.cursomc.repositories.ItemPedidoRepository;
 import com.william.cursomc.repositories.PagamentoRepository;
 import com.william.cursomc.repositories.PedidoRepository;
+import com.william.cursomc.security.UserSS;
+import com.william.cursomc.services.exceptions.AuthorizationException;
 import com.william.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,19 +28,19 @@ public class PedidoService {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
-	
+
 	@Autowired
 	private BoletoService boletoService;
-	
+
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
+
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-	
+
 	@Autowired
 	private ClienteService clienteService;
 
@@ -40,7 +48,7 @@ public class PedidoService {
 		return pedidoRepository.findById(id)
 				.orElseThrow(() -> new ObjectNotFoundException("Pedido não encontrado com o id: " + id));
 	}
-	
+
 	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
@@ -48,26 +56,36 @@ public class PedidoService {
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		obj.setCliente(clienteService.find(obj.getCliente().getId()));
-		
-		if(obj.getPagamento() instanceof PagamentoComBoleto) {
+
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
 			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
 			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
 		}
-		
+
 		obj = pedidoRepository.save(obj);
 		pagamentoRepository.save(obj.getPagamento());
-		
-		for(ItemPedido ip: obj.getItens()) {
+
+		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
 			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
-		
+
 		itemPedidoRepository.saveAll(obj.getItens());
-		
+
 		System.out.println(obj);
-		
+
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 }
